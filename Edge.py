7625ebfa2bf3cec4372ffb6e66e9295770e3c45d
arlekin147 <aapobejimov@gmail.com>
2019-04-27 18:05:50 +0300

    the first commit

---
+++ Edge.py
@@ -0,0 +1,17 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+class Edge(object):
+    def __init__(self, x1, y1, z1, x2, y2, z2, visible = True):
+        self.x1 = x1
+        self.y1 = y1
+        self.z1 = z1
+        self.x2 = x2
+        self.y2 = y2
+        self.z2 = z2
+        self.visible = visible
+
+    def Draw(self):
+        glVertex3f(self.x1, self.y1, self.z1) # 1 1 -1 -  0
+        glVertex3f(self.x2, self.y2, self.z2) # -1 1 -1 - 1
